f7bf974ded9ae625c21a165f0b56416c7b9c7eb7
Bryan Elliott <fordiman@gmail.com>
2019-03-24 15:48:33 -0400

    Implemented the button flasher, which I'd forgotten about.

--- MovieSign.ino
+++ MovieSign.ino
@@ -6,10 +6,12 @@
 #include <Bounce.h>
 
 #define BIG_RED_BUTTON         0               // big red button should bridge pin 0 and ground.
-#define FLASHER                1               // Flasher should be attached to digital pin 1
+#define BIG_RED_LED            1               // big red button's LED
+#define FLASHER                2               // Flasher should be attached to digital pin 2
 #define SIREN                  "MOVIESGN.WAV"  // Play this file off the SD card
 #define MAX_MOVIE_SIGN_MS      5000            // Don't let it go more than 5s
 #define MS_PER_LOOP            40              // Loop resolution is about 25 Hz.
+#define MS_PER_FLASH           500             // Time for 1 cycle of the flasher, about a half second
 
 // Define audio components
 AudioPlaySdWav           siren;
@@ -41,9 +43,15 @@ elapsedMillis blaringFor;
 // big red button.
 elapsedMillis loopCounter;
 
+// Timer for the flasher
+elapsedMillis bigRedLedState;
+
 void setup() {
   // Set up the pin for bigRedButton
   pinMode(BIG_RED_BUTTON, INPUT_PULLUP);
+
+  // Set up the pin controlling the button's LED
+  pinMode(BIG_RED_LED, OUTPUT);
 
   // Set up the pin controlling the flashy lamp relay
   pinMode(FLASHER, OUTPUT);
@@ -53,24 +61,29 @@ void setup() {
   Serial.println("Movie sign ready!");
 }
 
-// sets `state`, enables the flasher, starts playing, and resets the counter
+// sets `state`, enables the flashers, starts playing, and resets the counter
 void weveGotMovieSign() {
   Serial.println("WE'VE GOT MOVIE SIGN!");
   state = WEVE_GOT_MOVIE_SIGN;
+  digitalWrite(BIG_RED_LED, 1);
   digitalWrite(FLASHER, 1);
   siren.play(SIREN);
   blaringFor = 0;
+  bigRedLedState = 0;
 }
 
 // resets `state`, disables the flasher, and stops playback
 void noMoreMovieSign() {
   Serial.println("Movie sign: offline");
   state = OFF;
+  digitalWrite(BIG_RED_LED, 0);
   digitalWrite(FLASHER, 0);
   if (siren.isPlaying()) {
     siren.stop();
   }  
 }
+
+
 
 void loop() {
   // reset the timer for the loop
@@ -98,6 +111,19 @@ void loop() {
     Serial.println("Woop.  Play it again, Sam.");
     // loop that shit.
     siren.play(SIREN);
+  }
+  if (WEVE_GOT_MOVIE_SIGN) {
+    // Flash the LED
+    // "X >> 1" is a fast integer "X / 2"
+    if (bigRedLedState > (MS_PER_FLASH >> 1)) {
+      // We're in the second half of the flash cycle
+      digitalWrite(BIG_RED_LED, 0);      
+    } else if (bigRedLedState > MS_PER_FLASH) {
+      // We're at the end of the cycle
+      digitalWrite(BIG_RED_LED, 1);
+      // Reset the timer
+      bigRedLedState = 0;
+    }
   }
   // Let the processor chill for the remaining alloted time for this loop.
   delay(MS_PER_LOOP - loopCounter);
